936d423387ccac3c5f616f0e0352fe47d72149dd
Rajeime <rajeimeAmber@gmail.com>
2022-02-26 09:41:18 -0500

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faExclamationCircle  } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'google-form';
 
+  faerror = faExclamationCircle
   // show password by click action
   showPassword:string = "password"
 
@@ -21,4 +24,46 @@ export class AppComponent {
     }
   }
 
+  // grouping form inputs and validating it
+  loginForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required
+    ]),
+    lastName: new FormControl('',[
+      Validators.required
+    ]),
+    userName: new FormControl('',Validators.compose([
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30)
+    ])
+    ),
+    password: new FormControl(null, Validators.compose([
+      Validators.required
+    ])
+    ),
+    confirmPassword: new FormControl(null, Validators.required)
+  })
+
+  get firstName(){
+    return this.loginForm.get('firstName')
+  }
+
+  get lastName(){
+    return this.loginForm.get('lastName')
+  }
+
+  get userName(){
+    return this.loginForm.get('userName')
+  }
+
+  get password(){
+    return this.loginForm.get('password')
+  }
+
+  
+
+  submitted(){
+    console.log(this.loginForm.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleFormComponent } from './components/google-form/google-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    FontAwesomeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
